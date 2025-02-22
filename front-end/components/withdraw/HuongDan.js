c13d4dc1ca3a05ad5b6c7199d8bdd0f035e4aa6e
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2025-02-22 15:57:11 +0000

    Update HuongDan.js

--- front-end/components/withdraw/HuongDan.js
+++ front-end/components/withdraw/HuongDan.js
@@ -2,10 +2,10 @@ import { MIN_MONEY_WITHDRAW } from "@/configs/withdraw.config";
 import { convertJSXMoney } from "@/utils/convertMoney";
 import { Box, Typography } from "@mui/material";
 import Link from "next/link";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const HuongDan = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
   return (
     <>
@@ -21,14 +21,15 @@ const HuongDan = () => {
           color: "text.primary",
         }}
       >
-        <h2 className="title">{t('Withdraw guide')}</h2>
+        <h2 className="title">{t("Withdraw guide")}</h2>
         <Typography component="ul">
           <li>
-          {t('Withdraw guide step1-1')} <Link href="/password-withdraw">{t('Withdraw guide step1-2')}</Link> {t('Withdraw guide step1-3')}
+            {t("Withdraw guide step1-1")} <Link href="/password-withdraw">{t("Withdraw guide step1-2")}</Link>{" "}
+            {t("Withdraw guide step1-3")}
           </li>
-          <li>{t('Please enter your withdrawal password')}</li>
-          <li>{t('Withdraw guide step3')}</li>
-          <li>{t('Withdraw guide step4')}</li>
+          <li>{t("Please enter your withdrawal password")}</li>
+          <li>{t("Withdraw guide step3")}</li>
+          <li>{t("Withdraw guide step4")}</li>
         </Typography>
       </Box>
     </>
